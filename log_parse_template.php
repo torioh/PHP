ee256b8b4a47e9afa9e779a1add76160360ed941
y.ootori <y.ootori@geniee.co.jp>
2014-02-18 19:11:51 +0900

    refactor

--- log_parse_template.php
+++ log_parse_template.php
@@ -1,7 +1,7 @@
 <?php
 if($argc < 2) {
- echo "input file is required";
- die();
+    echo "input file is required\n";
+    die();
 }
 
 for($i = 1; $i < $argc; $i++) {
@@ -10,28 +10,22 @@ for($i = 1; $i < $argc; $i++) {
 }
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set("memory_limit", "1086M");
-$count = 0;
-
-$current_directory = getcwd();
-$output_file = $current_directory."/output_".$input[0];
-
-print $output_file;
-
-$output = array();
 
 foreach ($input as $file_name) {
 
-    // ƒtƒ@ƒCƒ‹“Çž‚Ý
     $fp = fopen ("$file_name", "r");
     if(!$fp) {
       echo "can not find input file:".$file_name."\n";
       die();
     }
+    $cnt = 1;
     while (!feof ($fp)) {
         $buffer = fgets($fp);
 
         // do something here
+            
+        $cnt++;
     }
+    fclose ($fp);
 }
 
-fclose ($fp);
